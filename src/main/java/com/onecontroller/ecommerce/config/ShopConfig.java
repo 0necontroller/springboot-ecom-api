fa3997a246f88d3773435db196a934c9bb22a66f
Abel Misiocha <wells.mail.13@gmail.com>
2025-12-02 14:56:10 +0300

    feat(core): Add model mapper dependency and config

---
+++ src/main/java/com/onecontroller/ecommerce/config/ShopConfig.java
@@ -0,0 +1,13 @@
+package com.onecontroller.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShopConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
